0ce5b146873e6f6b11ab493da70b3cc2745ddaf2
Angel Gutierrez <angelgutierrez@fb.com>
2020-06-26 14:46:52 -0700

    movie names in table view, no styling

---
+++ flix/View Controllers/MoviesViewController.h
@@ -0,0 +1,17 @@
+//
+//  MoviesViewController.h
+//  flix
+//
+//  Created by Angel Gutierrez on 6/26/20.
+//  Copyright © 2020 Angel Gutierrez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MoviesViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
